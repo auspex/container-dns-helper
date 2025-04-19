61d6b42cc2dfbdcee8f8f686c71c9a22c8f50cc7
Derek Broughton <auspex@pointerstop.ca>
2025-04-19 17:30:42 -0300

    Clean up network disconnection logic, add documentation

--- AddToDns.py
+++ AddToDns.py
@@ -1,15 +1,15 @@
-import os
+#! /usr/bin/python3
+ 
 import signal
-import sys
-import threading
 import asyncio
 
 import sdbus
 from docker import DockerClient
-from pyroute2 import IPRoute
+from pyroute2 import IPRoute, NDB
 from sdbus_async.networkmanager import (
     NetworkConnectionSettings,
     NetworkManager,
+    NetworkDeviceGeneric,
     NetworkManagerSettings,
     )
 from sdbus_async.networkmanager.settings import (
@@ -18,27 +18,36 @@ from sdbus_async.networkmanager.settings import (
     Ipv4Settings, 
     MacvlanSettings,
     )
+from sdbus.utils import parse
+
+from pyroute2.netlink.rtnl.ifinfmsg import ifinfmsg
 
 sdbus.set_default_bus(sdbus.sd_bus_open_system())
 
 ipr = IPRoute()
 docker = DockerClient(base_url='unix://var/run/docker.sock')
 NM = NetworkManager()
+
 class ShutdownRequested(BaseException): pass
 
 def signal_handler(sig, frame):
-    if sig in [signal.SIGKILL, signal.SIGINT]:
+    """
+    Exit cleanly on SIGTERM ("docker stop"), SIGINT (^C when interactive)
+    """
+    if sig in [signal.SIGINT, signal.SIGTERM]:
         raise ShutdownRequested
 
 def container_names():
-    ### get names of all Docker containers having the label `dhcp=true` and using a `bridge`
+    """
+    Get names of all Docker containers having the label `dhcp=true` and using a `bridge` network interface
+    """
     return set(
         container.name for container in docker.containers.list(filters={'label':'dhcp=true'})
     ).intersection(
         container.name for network in docker.networks.list(filters={'driver':'bridge'}, greedy=True) for container in network.containers
     )
 
-def default_route():
+def get_default_route():
     # ASSUME that get_default_routes actually returns routes in metric order
     # -- I'm not sure that's guaranteed!
     routes = ipr.get_default_routes()
@@ -48,40 +57,74 @@ def default_route():
         ifname = link[0].get('ifname')
     return ifname
 
-async def init():
-    containers = container_names()
-    parent = default_route()
-    if parent is not None:
-        [await publish_IP(parent, container) for container in containers]
-
-    try:
-        for event in docker.events(filters={'type':'network'}, decode=True):
-            if ShutdownRequested: 
-                break
-            attributes = event['Actor']['Attributes']
-            if attributes['type'] == 'bridge':
-                container = docker.containers.get(attributes['container'])
-                if container.labels['dhcp'] == 'true':
-                    if event['Action'] == 'connect':
-                        await publish_IP(default_route(), container.name)
-                    else:
-                        await unpublish_IP(container.name)
-    except ShutdownRequested:
-        print ('shutting down')
-
-    [await unpublish_IP(container) for container in container_names()]
-    ipr.release()
-    
-async def publish_IP(default_route, container):
+async def watch_for_disconnect(iface):
     """
-    Create a Macvlan connection named `container`, with `default_route` as the parent, 
+    Watch for disconnection on the default route. If it disconnects, signal RouteChanged to rebuild the macvlans
+
+    NB. This isn't working!
+    """
+    devices = [NetworkDeviceGeneric(x) for x in (await NM.get_devices())]
+    paths = [await x.active_connection for x in devices if await x.interface==iface]
+
+    # settings = NetworkManagerSettings()
+    # async for x in settings.connection_removed:
+    #     print(x)
+
+    connection = NetworkConnectionSettings(paths[0])
+    try:
+        async for x in connection.removed:
+            print (x)
+            # If the default route changes, just shut down the container, and let Docker restart it
+            raise ShutdownRequested
+    except asyncio.exceptions.CancelledError:
+        pass
+
+async def init():
+    """
+    - start a second task just to wait for a disconnect on the parent interface,
+    - create a NetworkManager connection for each required container
+    - watch the docker socket for network connect/disconnect events
+    - if the parent interface goes away, or the container is stopped, remove the NM connections & exit
+    """
+    parent = get_default_route()
+
+    if parent is not None:
+        task = asyncio.create_task(watch_for_disconnect(parent))
+        # try:
+        # except asyncio.CancelledError:
+        try:
+            [await publish_IP(parent, container) for container in container_names()]
+            await docker_event_loop(parent)
+        except ShutdownRequested:
+            task.cancel()
+            print ('shutting down')
+            [await unpublish_IP(container) for container in container_names()]
+            await task
+
+async def docker_event_loop(parent):
+    """
+    Watch for docker network events and add or remove containers to/from the host DNS as required.
+    """
+    for event in docker.events(filters={'type':'network'}, decode=True):
+        attributes = event['Actor']['Attributes']
+        if attributes['type'] == 'bridge':
+            container = docker.containers.get(attributes['container'])
+            if container.labels['dhcp'] == 'true':
+                if event['Action'] == 'connect':
+                    await publish_IP(parent, container.name)
+                else:
+                    await unpublish_IP(container.name)
+
+async def publish_IP(parent, container):
+    """
+    Create a Macvlan connection named `container`, with `parent` as the parent, 
     and get an IPv4 address via DHCP
 
     Functionally equivalent to:
-        nmcli connection add ifname {container} con-name {container} save no type macvlan dev {default_route} mode vepa -- +ipv4.dhcp-hostname {container}
+        nmcli connection add ifname {container} con-name {container} save no type macvlan dev {parent} mode vepa -- +ipv4.dhcp-hostname {container}
     """
-    print(f'nmcli connection add ifname {container} con-name {container} save no type macvlan dev {default_route} mode vepa -- +ipv4.dhcp-hostname {container}')
     connection_paths = await NetworkManagerSettings().get_connections_by_id(container)
+    path = ''
     if len(connection_paths) == 0:
         # create a new Macvlan device
         # # make the new connection Macvlan, with autoconnect
@@ -93,7 +136,7 @@ async def publish_IP(default_route, container):
                 interface_name=container
             ),
             macvlan=MacvlanSettings(
-                parent=default_route,
+                parent=parent,
                 mode=1 # vepa
             ),
             # # don't forget to tell NetworkManager to request an IP for this hostname
@@ -103,34 +146,38 @@ async def publish_IP(default_route, container):
                 )
         )
         path = await NetworkManagerSettings().add_connection_unsaved(profile.to_dbus())
-        # Now, NetworkManager should have a profile, but you still have to activate it
-        await NM.activate_connection(path)
     else:
         # modify existing device
         # (need to find the .../Settings object from the .../ActiveConnection object)
-        connection = NetworkConnectionSettings(connection_paths[0])
+        path = connection_paths[0]
+        connection = NetworkConnectionSettings(path)
 
         settings = await connection.get_settings()
         settings['connection']['id'] = container
         settings['connection']['interface-name'] = container
         settings['connection']['autoconnect'] = True
-        settings['macvlan']['parent'] = default_route
+        settings['macvlan']['parent'] = parent
         settings['ipv4']['dhcp-hostname'] = container
         settings.update()
         # might need to deactivate before re-activating
-        await NM.activate_connection(connection_paths[0])
+        
+    # Now, NetworkManager should have a profile, but you still have to activate it
+    new_path = await NM.activate_connection(path)
+    print(f'add ifname {container} dev {parent}: {new_path}')
+    return new_path
 
 async def unpublish_IP(container):
-    print(f'nmcli connection delete {container}')
     connection_paths = await NetworkManagerSettings().get_connections_by_id(container)
     if len(connection_paths) > 0:
-        connection = NetworkConnectionSettings(connection_paths[0])
+        path = connection_paths[0]
+        connection = NetworkConnectionSettings(path)
         await connection.delete()
+        print(f'delete {container}: {path}')
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
+
     asyncio.run(init())
-    # init()
-    
-    # ipr.register_callback(network_changed)
+
+    ipr.release()
--- README.md
+++ README.md
@@ -4,19 +4,18 @@ A simple NetworkManager helper to insert all of your Docker container names into
 
 ## Rationale
 
-I want to be able to find any container on my host machine by name just as I would find the host itself. 
+I want to be able to find any container on my host machine by name just as I would find the host itself.  
 
-There are a number of methods available to create mDNS names for your docker containers. These would be aliases for the host machine. `hardillb/traefik-avahi-helper` was the best I found, 
-but in my experience mDNS was less than robust (that's no slur on the the above software, but problems with mDNS). 
-Sometimes the names wouldn't be published. At others, I could see the names from my local server but not from any other machine on the LAN. It's probably my misconfiguration, but I found it 
-frustrating, and figured there had to be an easy way to get my local router to add an 'alias'. But, it's **not** simple. If you tell DHCP to assign an IP for a new host name, it'll match on 
+There are a number of methods available to create mDNS names for your docker containers. These would be aliases for the host machine. `hardillb/traefik-avahi-helper` was the best I found,
+but in my experience mDNS was less than robust (that's no slur on the the above software, but problems with mDNS).
+Sometimes the names wouldn't be published. At others, I could see the names from my local server but not from any other machine on the LAN. It's probably my misconfiguration, but I found it
+frustrating, and figured there had to be an easy way to get my local router to add an 'alias'. But, it's **not** simple. If you tell DHCP to assign an IP for a new host name, it'll match on
 the interface's MAC address, and just rename your machine, rather than creating a new one.
 
-This approach uses NetworkManager's `nmcli` to create a new `macvlan` interface as a child of your primary Internet-facing ethernet NIC, for any container with a `label` of `dhcp=true`. A `macvlan` interface has a new MAC address, 
-different from anything existing on your host. Therefore, when NetworkManager brings it up, it will get assigned a new IP—even though traffic will actually flow through the parent interface.
+This approach uses `python-sdbus-networkmanager` to have the host's NetworkManager create a new `macvlan` interface as a child of your primary Internet-facing ethernet NIC, for any container with a `label` of `dhcp=true`. A `macvlan` interface has a new MAC address, different from anything existing on your host. Therefore, when NetworkManager brings it up, it will get assigned a new IP—even though traffic will actually flow through the parent interface.
 
 So, if my host machine is named **server.home** and has an IP address of 192.168.1.10, a container named **mycontainer** might be given the name **mycontainer.home** and assigned 192.168.1.15.
-The Avahi/mDNS method would have made them **server.local** and **mycontainer.local** and both names would translate to 192.168.1.10, but the important detail is that in either case, everything is 
+The Avahi/mDNS method would have made them **server.local** and **mycontainer.local** and both names would translate to 192.168.1.10, but the important detail is that in either case, everything is
 sent to the physical interface that has the IP 192.168.1.10 and standard reverse proxies can route to the correct container.
 
 ## Getting Started
@@ -30,7 +29,6 @@ cd container-dns-helper
 
 In order to run this container you'll need docker installed.
 
-* [Windows](https://docs.docker.com/windows/started)
 * [OS X](https://docs.docker.com/mac/started/)
 * [Linux](https://docs.docker.com/linux/started/)
 
@@ -51,15 +49,15 @@ docker compose up -d --build
 #### Container Parameters
 
 The compose file runs with `security_opt=apparmor:unconfined` to permit communication with DBUS.
-Also, if the host interface is delete for any reason other than being stopped by NetworkManager, `nmcli` is unable to cleanup the macvlan interfaces. Consequently we need `cap_add=['NET_ADMIN','NET_RAW']` to allow the container to delete the macvlan interfaces if necessary. 
 
 #### Environment Variables
+
 None yet
 
 #### Volumes
 
 * `/var/run/docker.sock` - Needed for access to the Docker container and network information
-* `/var/run/dbus` - so that `nmcli`  can communicate with the host's NetworkManager
+* `/var/run/dbus` - so that `python-sdbus-networkmanager` can communicate with the host's NetworkManager
 
 ## ToDo
 
@@ -68,7 +66,7 @@ work on any router with a properly compliant DHCP server.
 
 ## Built With
 
-* docker.io/library/python:3.13.0 
+* docker.io/library/python:3.13.3-slim
 
 ## Find Us
 
